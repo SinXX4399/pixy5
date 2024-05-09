87c091a1c60ddc460339c75ff2199e9ae28eae28
SinXX4399 <seniarue@gmail.com>
2024-05-10 01:12:01 +0700

    initial commit

--- src/app/api/webhooks/clerk/route.ts
+++ src/app/api/webhooks/clerk/route.ts
@@ -1,4 +1,3 @@
-/* eslint-disable camelcase */
 import { clerkClient } from "@clerk/nextjs";
 import { WebhookEvent } from "@clerk/nextjs/server";
 import { headers } from "next/headers";
@@ -59,13 +58,14 @@ export async function POST(req: Request) {
 
   // CREATE
   if (eventType === "user.created") {
-    const { id, email_addresses, image_url,username } = evt.data;
+    const { id, email_addresses, image_url, first_name, last_name, username } = evt.data;
 
     const user = {
       clerkId: id,
       email: email_addresses[0].email_address,
       username: username!,
-
+      firstName: first_name,
+      lastName: last_name,
       photo: image_url,
     };
 
@@ -85,10 +85,11 @@ export async function POST(req: Request) {
 
   // UPDATE
   if (eventType === "user.updated") {
-    const { id, image_url, username } = evt.data;
+    const { id, image_url, first_name, last_name, username } = evt.data;
 
     const user = {
-     
+      firstName: first_name,
+      lastName: last_name,
       username: username!,
       photo: image_url,
     };
