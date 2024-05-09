4c4e60a325ac90b48408a00446814a7c6c6bacc9
SinXX4399 <seniarue@gmail.com>
2024-05-10 02:28:54 +0700

    Update code with new features

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,8 +1,10 @@
 import { authMiddleware } from "@clerk/nextjs";
 
 export default authMiddleware({
-  ignoredRoutes: ["/api/webhooks(.*)"],
-});
+ 
+    publicRoutes: ['/', '/api/webhooks/clerk']
+  });
+   
 
 export const config = {
   matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
